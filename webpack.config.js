9cf458dba45cecb2fcec8dc704bf34ae1efd3aef
lenais <lenais.desbos@yahoo.fr>
2023-05-30 07:57:33 +0200

    quatre

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.html',
+  entry: './index.html',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
